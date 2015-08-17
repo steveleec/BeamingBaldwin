d34352590f7052fe8e4723927321e5a205139f88
Christopher Yu <cyu06@hotmail.com>
2015-08-17 10:58:51 -0700

    [feature] Added redirect to home after signout after 2 seconds per CHa's request

--- client/js/components/Signout.js
+++ client/js/components/Signout.js
@@ -1,18 +1,35 @@
 var React = require('react');
+var Router = require('react-router');
 
 var auth = require('../services/auth');
 
 var Signout = React.createClass({
+  mixins: [Router.Navigation],
+
   statics: {
     willTransitionTo: function(transition) {
       if (!auth.loggedIn()) {
         transition.redirect('/');
       }
     },
+
+    attemptedTransition: null,
+
   },
 
   componentDidMount: function() {
-    auth.logout();
+    auth.logout(function() {
+      var transition;
+      if (Signout.attemptedTransition) {
+        transition = Signout.attemptedTransition;
+        Signout.attemptedTransition = null;
+        transition.retry();
+      } else {
+        setTimeout(function() {
+          this.replaceWith('/');
+        }.bind(this), 2000);
+      }
+    }.bind(this));
   },
 
   render: function() {
@@ -20,6 +37,7 @@ var Signout = React.createClass({
       <div className="Signout__container">
         <div className="Signout">
           <h2 className="Signout__h2">You are now logged out.</h2>
+          <h3 className="Signout__h3">Redirecting you back to home in two seconds.</h3>
         </div>
       </div>
     );
